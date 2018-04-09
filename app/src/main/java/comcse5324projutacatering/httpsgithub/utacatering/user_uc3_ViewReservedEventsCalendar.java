1e71268a1d7ffeb0b8e2c165a8784e75f46bf86f
jro2018tx <jro2018tx@gmail.com>
2018-04-09 15:33:12 -0500

    -added blank user activities for uc3,4,5.

---
+++ app/src/main/java/comcse5324projutacatering/httpsgithub/utacatering/user_uc3_ViewReservedEventsCalendar.java
@@ -0,0 +1,13 @@
+package comcse5324projutacatering.httpsgithub.utacatering;
+//TODO .. look into android provided calendar layout
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class user_uc3_ViewReservedEventsCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_uc3_view_reserved_events_calendar);
+    }
+}
---
+++ app/src/main/java/comcse5324projutacatering/httpsgithub/utacatering/user_uc4_5_ViewReservedEventAndCancel.java
@@ -0,0 +1,13 @@
+package comcse5324projutacatering.httpsgithub.utacatering;
+//TODO
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class user_uc4_5_ViewReservedEventAndCancel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_uc4_5_view_reserved_event_and_cancel);
+    }
+}
