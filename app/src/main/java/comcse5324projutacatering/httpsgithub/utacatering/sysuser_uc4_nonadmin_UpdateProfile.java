5305989c30abc64c330416e03b7cc04f4137b949
jro2018tx <jro2018tx@gmail.com>
2018-04-09 01:05:00 -0500

    -staging for UpdateProfile page.

---
+++ app/src/main/java/comcse5324projutacatering/httpsgithub/utacatering/sysuser_uc4_nonadmin_UpdateProfile.java
@@ -0,0 +1,13 @@
+package comcse5324projutacatering.httpsgithub.utacatering;
+//TODO fill out and make sure non-admin home pages connect appropriately
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sysuser_uc4_nonadmin_UpdateProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sysuser_uc4_nonadmin_update_profile);
+    }
+}
