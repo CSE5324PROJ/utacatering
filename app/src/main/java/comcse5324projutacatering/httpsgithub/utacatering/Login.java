3924f8260695ee29a0b4c7f59202fa90ecefabca
jro2018tx <jro2018tx@gmail.com>
2018-04-08 23:24:58 -0500

    -clicking background on Login screen will clear focus from editText fields and drop keyboard.

--- app/src/main/java/comcse5324projutacatering/httpsgithub/utacatering/Login.java
+++ app/src/main/java/comcse5324projutacatering/httpsgithub/utacatering/Login.java
@@ -1,6 +1,7 @@
 package comcse5324projutacatering.httpsgithub.utacatering;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -104,5 +106,13 @@ public class Login extends Activity {
                 return false;
             }
         });
+    }
+    public void hideKeyboard(View view){
+        if(view!=null){
+            InputMethodManager inMethMan = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            if(inMethMan!=null){
+                inMethMan.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        }
     }
 }
