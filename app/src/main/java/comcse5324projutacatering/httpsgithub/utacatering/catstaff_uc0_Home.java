b10bc7de8848167d0e2a955a518ae7485c8c7af9
pallabee-mahanta <37675632+pallabee-mahanta@users.noreply.github.com>
2018-04-10 23:01:19 -0500

    caterer staff home added intent to update profile added menubar

--- app/src/main/java/comcse5324projutacatering/httpsgithub/utacatering/catstaff_uc0_Home.java
+++ app/src/main/java/comcse5324projutacatering/httpsgithub/utacatering/catstaff_uc0_Home.java
@@ -1,9 +1,11 @@
 package comcse5324projutacatering.httpsgithub.utacatering;
+//TODO viewEventList
 
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -29,5 +31,36 @@ public class catstaff_uc0_Home extends Activity {
                 //startActivity(intent);
             }
         });
+
+        updateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v)  {
+                Intent intent =  new Intent(catstaff_uc0_Home.this, sysuser_uc4_nonadmin_UpdateProfile.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.main_menu_sign_out:
+                Intent intent0 = new Intent(catstaff_uc0_Home .this, sysuser_uc2_Login .class);
+                intent0.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                );
+                startActivity(intent0);
+                finish();
+                return true;
+            case R.id.main_menu_go_home:
+                Intent intent = new Intent(catstaff_uc0_Home.this, catstaff_uc0_Home.class);
+                startActivity(intent);
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 }
