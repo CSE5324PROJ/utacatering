3fb3a426eee4caf91d862c84e21f030ecf2252b8
pallabee-mahanta <37675632+pallabee-mahanta@users.noreply.github.com>
2018-04-10 23:11:03 -0500

    caterer staff menu bar added

--- app/src/main/java/comcse5324projutacatering/httpsgithub/utacatering/catstaff_uc0_Home.java
+++ app/src/main/java/comcse5324projutacatering/httpsgithub/utacatering/catstaff_uc0_Home.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +41,13 @@ public class catstaff_uc0_Home extends Activity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
